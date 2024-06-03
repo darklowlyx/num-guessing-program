fab5de2ff9ffbea4c577327b732d0151209f50aa
darklowlyx <dark.codx.lowly@gmail.com>
2024-06-03 21:07:16 +0500

    version 0.3
    big update with new functionality; prompt is from input

--- js/script.js
+++ js/script.js
@@ -1,34 +1,27 @@
 const minNum = 1;
 const maxNum = 100;
 const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-console.log(answer)
+console.log(answer); // для тестирования, уберите или закомментируйте в реальной игре
 let attempts = 0;
-let guess;
-let running = true;
+const result = document.getElementById("result");
+const input = document.getElementById("input");
+const submitBtn = document.getElementById("submit");
 
-while (running) {
-	guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-	guess = Number(guess);
-	if (isNaN(guess)) {
-		window.alert("Please enter a valid number");
-	}
-	else if (guess < minNum || guess > maxNum) {
-		window.alert("Please enter a valid number");
-	}
-	else {
-		attempts++;
-		if (guess < answer) {
-			window.alert("TOO LOW! TRY AGAIN!");
-		}
-		else if (guess > answer) {
-			window.alert("TOO HIGH! TRY AGAIN!");
-		}
-		else {
-			window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
-			running = false;
-		}
-
-	}
-
-}
-
+submitBtn.addEventListener("click", () => {
+  let guess = Number(input.value);
+  if (isNaN(guess)) {
+      result.textContent = "Please enter a valid number";
+  } else if (guess < minNum || guess > maxNum) {
+      result.textContent = `Please enter a number between ${minNum} and ${maxNum}`;
+  } else {
+      attempts++;
+      if (guess < answer) {
+          result.textContent = "TOO LOW! TRY AGAIN!";
+      } else if (guess > answer) {
+          result.textContent = "TOO HIGH! TRY AGAIN!";
+      } else {
+          result.textContent = `CORRECT! The answer was ${answer}. It took you ${attempts} attempts`;
+          submitBtn.disabled = true;
+      }
+  }
+});
